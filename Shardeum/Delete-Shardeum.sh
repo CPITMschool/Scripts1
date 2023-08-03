b3fb3f213fbae1edc293cd72e316a69d8cdf4a16
CPITMschool <140173866+CPITMschool@users.noreply.github.com>
2023-08-03 22:08:53 +0300

    Update Delete-Shardeum.sh

--- Shardeum/Delete-Shardeum.sh
+++ Shardeum/Delete-Shardeum.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
 function logo() {
-bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+  bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
 function delete() {
-reset
-rm -f $HOME/.sdd_Shardeum_do_not_remove
-cd $HOME/.shardeum
-docker compose down
-cd $HOME
-rm -Rf $HOME/.shardeum
+  docker stop shardeum-dashboard && docker rm shardeum-dashboard
+  rm -rf .shardeum/
 }
 
-logo
 if [ -f $HOME/.sdd_Shardeum_do_not_remove ]; then
   delete
-  logo
 fi
+
+logo
+delete
+
+printGreen "Shardeum node видалено"
