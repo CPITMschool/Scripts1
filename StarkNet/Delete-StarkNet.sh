1020af6d3421646090a53c5ce32fd57cbf9bb268
CPITMschool <140173866+CPITMschool@users.noreply.github.com>
2023-08-03 22:10:59 +0300

    Update Delete-StarkNet.sh

--- StarkNet/Delete-StarkNet.sh
+++ StarkNet/Delete-StarkNet.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
 function logo() {
-bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+  bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
 function delete() {
-echo "-----------------------------------------------------------------------------"
-sudo systemctl stop starknetd
-sudo systemctl disable starknetd
-sudo rm -rf $HOME/pathfinder/py
-sudo rm -rf /etc/systemd/system/starknetd.service
-rm -rf /usr/local/bin/pathfinder
-echo "Нода успішно видалена"
-echo "-----------------------------------------------------------------------------"
+ systemctl stop starknetd
+  systemctl disable starknetd
+  rm -rf ~/pathfinder/
+  rm -rf /etc/systemd/system/starknetd.service
+  rm -rf /usr/local/bin/pathfinder
 }
 
-logo
 if [ -f $HOME/.sdd_StarkNet_do_not_remove ]; then
   delete
-  logo
 fi
+
+logo
+delete
+
+printGreen "StarkNet node видалено"
 
