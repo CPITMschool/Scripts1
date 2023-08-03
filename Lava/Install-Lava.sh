0ed0ff0b2771b3cd8354755db525f60e9d1c4fd5
CPITMschool <140173866+CPITMschool@users.noreply.github.com>
2023-08-03 17:20:34 +0300

    Update Install-Lava.sh

--- Lava/Install-Lava.sh
+++ Lava/Install-Lava.sh
@@ -1,73 +1,103 @@
 #!/bin/bash
 
-function logo() {
-bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+function install() {
+function printDelimiter {
+  echo "==========================================="
 }
 
-function install() {
-sudo apt --fix-broken install
-sudo apt-get update && sudo apt-get upgrade -y
-sudo dpkg --configure -a
-sudo apt-get install -f -y
-sudo apt install make clang git pkg-config libssl-dev build-essential gcc chrony curl jq ncdu bsdmainutils htop net-tools lsof fail2ban wget -y
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
 
-ver="1.19.4" && \
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
-sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
-rm "go$ver.linux-amd64.tar.gz" && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
-source $HOME/.bash_profile && \
-go version
-git clone https://github.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T
-wget https://lava-binary-upgrades.s3.amazonaws.com/testnet/v0.3.0/lavad
-chmod +x lavad
-mv lavad /usr/local/bin/
-LAVA_CHAIN="lava-testnet-1"
+source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 
-if [[ -z "$monikername" ]]; then
-  read -p "Enter a moniker name for your node: " _monikername
-  export monikername=$_monikername
-fi
+source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Ports.sh) && sleep 3
+export -f selectPortSet && selectPortSet
 
-LAVA_MONIKER="$monikername"
+read -r -p "Enter node moniker: " NODE_MONIKER
 
-if [[ -z "$walletname" ]]; then
-  read -p "Enter a wallet name for your node: " _walletname
-  export walletname=$_walletname
-fi
+CHAIN_ID=lava-testnet-1
+echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.profile
+source $HOME/.profile
 
-LAVA_WALLET="$walletname"
-echo 'export LAVA_CHAIN='${LAVA_CHAIN} >> $HOME/.bash_profile
-echo 'export LAVA_MONIKER='${LAVA_MONIKER} >> $HOME/.bash_profile
-echo 'export LAVA_WALLET='${LAVA_WALLET} >> $HOME/.bash_profile
-source $HOME/.bash_profile
-lavad init $LAVA_MONIKER --chain-id=lava-testnet-1
-lavad config chain-id $LAVA_CHAIN
-cp $HOME/GHFkqmTzpdNLDd6T/testnet-1/genesis_json/genesis.json $HOME/.lava/config
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025ulava\"/;" ~/.lava/config/app.toml
-seeds="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656"
-sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.lava/config/config.toml
-tee /etc/systemd/system/lavad.service > /dev/null <<EOF
+source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Dependencies.sh)
+
+echo "" && printGreen "Building binaries..." && sleep 1
+
+cd $HOME || return
+rm -rf lava
+git clone https://github.com/lavanet/lava
+cd lava
+latestTag=$(curl -s https://api.github.com/repos/lavanet/lava/releases/latest | grep '.tag_name'|cut -d\" -f4)
+echo $latestTag
+git checkout $latestTag
+make install && sleep 3
+cd ~
+
+sleep 1
+lavad config keyring-backend os
+lavad config chain-id $CHAIN_ID
+lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+rm -rf GHFkqmTzpdNLDd6T
+git clone https://github.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T.git && sleep 10
+sudo mv GHFkqmTzpdNLDd6T/testnet-1/genesis_json/genesis.json .lava/config
+
+CONFIG_TOML=$HOME/.lava/config/config.toml
+PEERS=""
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
+SEEDS="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656"
+sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
+
+APP_TOML=$HOME/.lava/config/app.toml
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
+sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
+sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 1000|g' $APP_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025ulava"|g' $APP_TOML
+
+# Customize ports
+CLIENT_TOML=$HOME/.lava/config/client.toml
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $CLIENT_TOML
+
+printGreen "Starting service and synchronization..." && sleep 1
+
+sleep 10
+
+sudo cp $HOME/.lava/data/priv_validator_state.json $HOME/.lava/priv_validator_state.json.backup && sleep 10
+sudo rm -rf $HOME/.lava/data
+curl -L https://snapshots.kjnodes.com/lava-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.lava && sleep 10
+sudo mv $HOME/.lava/priv_validator_state.json.backup $HOME/.lava/data/priv_validator_state.json
+
+sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
 [Unit]
-Description=lavad
+Description=Lava Node
 After=network-online.target
 [Service]
 User=$USER
 ExecStart=$(which lavad) start
 Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
+RestartSec=10
+LimitNOFILE=10000
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl daemon-reload
-systemctl enable lavad
-systemctl restart lavad
+
+sudo systemctl daemon-reload
+sudo systemctl enable lavad
+sudo systemctl start lavad
+
+printDelimiter
+printGreen "Check logs:            sudo journalctl -u lavad -f -o cat"
+printGreen "Check synchronization: lavad status 2>&1 | jq .SyncInfo.catching_up"
+printDelimiter
 
 }
-
-logo
 install
-touch $HOME/.sdd_Lava_do_not_remove
-logo
