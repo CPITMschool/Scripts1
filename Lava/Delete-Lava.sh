88b2e7e5b026d87f5a1243ad4bfdef62d62319e3
CPITMschool <140173866+CPITMschool@users.noreply.github.com>
2023-08-09 16:09:23 +0300

    Update Delete-Lava.sh

--- Lava/Delete-Lava.sh
+++ Lava/Delete-Lava.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
 function logo() {
   bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
 function delete() {
-function printGreen {
-  echo -e "\e[1m\e[32m${1}\e[0m"
-}
   sudo systemctl stop lavad
   sudo systemctl disable lavad
   sudo rm -rf $HOME/.lava
+  sudo rm -rf $HOME/lava
   sudo rm -rf $HOME/lavad
   sudo rm -rf /etc/systemd/system/lavad.service
   sudo rm -rf /usr/local/bin/lavad
@@ -22,4 +24,6 @@ if [ -f $HOME/.sdd_Lava_do_not_remove ]; then
 fi
 
 logo
+delete
+
 printGreen "Lava node видалено"
