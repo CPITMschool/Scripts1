3e2d08cb3a5b3722910f4d28c24c741b14fc044f
CPITMschool <140173866+CPITMschool@users.noreply.github.com>
2023-08-15 14:34:00 +0300

    Update Update-Nibiru.sh

--- Nibiru/Update-Nibiru.sh
+++ Nibiru/Update-Nibiru.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
 function logo() {
-bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+    bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
-function update() {
-sudo apt update && sudo apt upgrade -y
+function printGreen {
+    echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
 logo
-if [ -f $HOME/.sdd_Nibiru_do_not_remove ]; then
-  update
-  logo
-fi
+
+nibid_version=$(nibid version)
+printGreen "Версія вашої ноди Nibiru - $nibid_version"
+
+printGreen "Актуальна версія для оновлення v0.19.2. Бажаєте оновити? (Y/N)"
+read response
+
+function update() {
+    if [[ $response == "Y" || $response == "y" ]]; then
+        cd
+        git clone https://github.com/NibiruChain/nibiru
+        cd nibiru
+        git checkout v0.19.2
+        make install
+        
+        updated_version=$(nibid version)
+        printGreen "Оновлення до версії $updated_version завершено."
+    else
+        printGreen "Оновлення не виконано."
+    fi
+}
+
+update
