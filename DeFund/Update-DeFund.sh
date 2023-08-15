692882908b1ba71ee7faa5aeba9563c19c439ecc
CPITMschool <140173866+CPITMschool@users.noreply.github.com>
2023-08-15 14:32:19 +0300

    Update Update-DeFund.sh

--- DeFund/Update-DeFund.sh
+++ DeFund/Update-DeFund.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 function logo() {
-bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+    bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
-function update() {
-cd $HOME
-systemctl stop defund
-cd $HOME/defund
-git checkout v0.2.4
-make install
-systemctl restart defund
-cd $HOME
+function printGreen {
+    echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
 logo
-if [ -f $HOME/.sdd_Defund_do_not_remove ]; then
-  update
-  logo
-fi
+
+printGreen "Оновлення ноди."
+
+function update() {
+    printGreen "Починаємо оновлення..."
+    cd $HOME
+    systemctl stop defund
+    cd $HOME/defund
+    git checkout v0.2.6
+    make install
+    systemctl restart defund
+    cd $HOME
+    printGreen "Оновлення завершено."
+}
+
+update
