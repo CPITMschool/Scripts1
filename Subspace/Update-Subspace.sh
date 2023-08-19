e12721325af9755656b071ac61f792281617f617
Asapov <137000528+asapov01@users.noreply.github.com>
2023-08-20 00:31:54 +0300

    Update Update-Subspace.sh

--- Subspace/Update-Subspace.sh
+++ Subspace/Update-Subspace.sh
@@ -1,45 +1,96 @@
 #!/bin/bash
 
 function logo() {
-    bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
 function printGreen {
-    echo -e "\033[1;32m${1}\033[0m"
+  echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
 logo
 
-VERSION=$(awk '
-    /flags/ {
-        if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1
-        if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2
-        if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3
-        if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4
-        if (level > 0) { print level; exit level + 1 }
-        exit 1
-    }
-' /proc/cpuinfo)
-
-if [[ $VERSION -ne 2 && $VERSION -ne 3 ]]; then
-    printGreen "Our script doesn't support your processor"
-    exit
+function update() {
+printGreen "Видалення застарілої версії мережі Subspace Gemini 3e" && sleep 2
+sudo systemctl stop subspaced 
+sudo systemctl disable subspaced
+sudo rm -rf ~/.local/share/subspace*
+sudo rm -rf /etc/systemd/system/subspace*
+sudo rm -rf /usr/local/bin/subspace*
+  
+printGreen "Розпочалось встановлення Subpsace Gemini 3f"
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
 fi
 
-function update() {
-    echo "-----------------------------------------------------------------------------"
-    sudo systemctl stop subspaced
+sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
+cd $HOME
+wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.0-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.0-alpha
+sudo chmod +x pulsar
+sudo mv pulsar /usr/local/bin/
+sudo rm -rf $HOME/.config/pulsar
+/usr/local/bin/pulsar init
+#systemctl stop subspaced subspaced-farmer &>/dev/null
+#rm -rf ~/.local/share/subspace*
 
-    cd "$HOME" || return
-    wget -O subspace-cli https://github.com/subspace/subspace-cli/releases/download/v0.5.3-alpha-2/subspace-cli-ubuntu-x86_64-v2-v0.5.3-alpha-2
-    sudo chmod +x subspace-cli
-    sudo rm /usr/local/bin/subspace-cli
-    sudo mv subspace-cli /usr/local/bin/
+#source ~/.bash_profile
+sleep 1
 
-    source "$HOME/.bash_profile"
-    sudo systemctl restart subspaced
+echo "[Unit]
+Description=Subspace Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/pulsar farm --verbose
+Restart=on-failure
+LimitNOFILE=1024000
+
+[Install]
+WantedBy=multi-user.target" > $HOME/subspaced.service
+
+sudo mv $HOME/subspaced.service /etc/systemd/system/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable subspaced
+sudo systemctl restart subspaced
+
+if [[ `service subspaced status | grep active` =~ "running" ]]; then
+echo ""
+  echo "=================================================="
+  printGreen "Subspace Gemini 3f успішно встановлено"
+  echo ""
+  printGreen "Корисні команди:"
+  echo "Перевірити статус ноди - systemctl status subspaced"
+  echo "Журнал логів - journalctl -u subspaced -f -o cat"
+  echo "=================================================="
+  echo ""
+else
+  printGreen "Нода Subspace не встановлено, спробуйте встановити ще раз."
+fi
+
+touch $HOME/.sdd_Subspace_do_not_remove
+
 }
 
-update
-printGreen "Your node has been updated successfully"
-printGreen "Check your node status: journalctl -u subspaced -f -o cat"
+printGreen "Під час встановлення ваша нода видалиться та перевстановиться на актуальну мережу Gemini 3f. Ви згідні? (Y/N): "
+read choice
+
+if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
+  update
+elif [[ "$choice" == "N" || "$choice" == "n" ]]; then
+  printGreen "Ви відмовилися від перевстановлення ноди."
+else
+  printGreen "Невірний вибір. Будь ласка, введіть Y або N."
+fi
