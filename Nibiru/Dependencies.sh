2c0bd28d5607b9e583980eba51e8e4245a6b7cc3
Asapov <137000528+asapov01@users.noreply.github.com>
2023-08-25 13:38:42 +0300

    Update Dependencies.sh

--- Nibiru/Dependencies.sh
+++ Nibiru/Dependencies.sh
@@ -4,15 +4,13 @@ function printGreen {
   echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
-printGreen "Installing dependencies..." && sleep 1
+printGreen "Оновлення пакетів сервера..." && sleep 1
 sudo apt-get update &&
 sudo apt-get install -y curl iptables build-essential git lz4 wget jq make gcc nano chrony \
 tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev &&
 
-printGreen "Installing go..." && sleep 1
+printGreen "Встановлюємо GO..." && sleep 1
 if ! [ -x "$(command -v go)" ]; then
   source <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/go_install.sh")
   source $HOME/.bash_profile
 fi
-
-printGreen "$(go version)"
