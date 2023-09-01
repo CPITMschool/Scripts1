b3f1ddc70d21c01a8bf60792cbe366fd0632a407
Asapov <137000528+asapov01@users.noreply.github.com>
2023-09-01 12:46:53 +0300

    Update Update-StarkNet.sh

--- StarkNet/Update-StarkNet.sh
+++ StarkNet/Update-StarkNet.sh
@@ -4,39 +4,39 @@ function logo() {
 bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
 function update() {
-echo "-----------------------------------------------------------------------------"
-echo "Завантаження залежностей"
+clear
+logo
+printGreen "Завантаження залежностей"
 bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/main.sh) &>/dev/null
 bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Install-Docker.sh) &>/dev/null
-echo "-----------------------------------------------------------------------------"
-echo "Оновлюємо репозиторій"
-echo "-----------------------------------------------------------------------------"
+printGreen "Оновлюємо репозиторій"
 cd $HOME/pathfinder
 git fetch
 git checkout `curl https://api.github.com/repos/eqlabs/pathfinder/releases/latest -s | jq .name -r`
-echo "-----------------------------------------------------------------------------"
-echo "Зупиняємо стару версію StarkNet, запущену через systemd"
-echo "-----------------------------------------------------------------------------"
+printGreen "Зупиняємо стару версію StarkNet"
 sudo systemctl stop starknet &>/dev/null
 sudo systemctl disable starknet &>/dev/null
 rm -rf $HOME/pathfinder/py/.venv &>/dev/null
-echo "-----------------------------------------------------------------------------"
-echo "Створюємо env файл зі змінною Alchemy або infura"
-echo "-----------------------------------------------------------------------------"
+printGreen "Створюємо env файл зі змінною Alchemy або infura"
 source $HOME/.bash_profile
 echo "PATHFINDER_ETHEREUM_API_URL=$ALCHEMY_KEY" > pathfinder-var.env
-echo "-----------------------------------------------------------------------------"
-echo "Завантажуємо останню версію docker image"
+printGreen "Завантажуємо останню версію docker image"
 docker-compose pull
-echo "Завантажили, переходимо до запуску"
-echo "-----------------------------------------------------------------------------"
+printGreen "Завантажили, переходимо до запуску"
 mkdir -p $HOME/pathfinder/pathfinder
 chown -R 1000.1000 .
 sleep 1
 docker-compose up -d
-echo "Нода оновлена і запущена"
-echo "-----------------------------------------------------------------------------"
+echo ""
+echo "==========================================="
+printGreen  "Нода оновлена і запущена"
+Перевірити версію ноди: docker exec -it pathfinder-starknet-node-1 pathfinder -V
+echo "==========================================="
 }
 
 update
