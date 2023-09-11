0e5c86dec76307fbdfdd077b3739c119e9a49444
Asapov <137000528+asapov01@users.noreply.github.com>
2023-09-12 01:01:03 +0300

    Update Install-Lava.sh

--- Lava/Install-Lava.sh
+++ Lava/Install-Lava.sh
@@ -12,70 +12,99 @@ function printGreen {
 
 source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 
-echo ""
 printGreen "Введіть ім'я для вашої ноди:"
-read -r NODE_MONIKER
+read -r MONIKER
 
-CHAIN_ID=lava-testnet-2
-echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.profile
-source $HOME/.profile
+printGreen "Встановлення необхідних залежностей"
+sudo apt -q update
+sudo apt -qy install curl git jq lz4 build-essential
+sudo apt -qy upgrade
 
-source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Dependencies.sh)
+printGreen "Встановлення Go"
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.20.8.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
-git clone https://github.com/lavanet/lava-config.git
-cd lava-config/testnet-2
-source setup_config/setup_config.sh
+cd $HOME
+rm -rf lava
+git clone https://github.com/lavanet/lava.git
+cd lava
+git checkout v0.22.0
 
-echo "Lava config file path: $lava_config_folder"
-mkdir -p $lavad_home_folder
-mkdir -p $lava_config_folder
-cp default_lavad_config_files/* $lava_config_folder
+export LAVA_BINARY=lavad
+make build
 
+mkdir -p $HOME/.lava/cosmovisor/genesis/bin
+mv build/lavad $HOME/.lava/cosmovisor/genesis/bin/
+rm -rf build
 
-lavad_binary_path="$HOME/go/bin/"
-mkdir -p $lavad_binary_path
-wget https://lava-binary-upgrades.s3.amazonaws.com/testnet-2/genesis/lavad
-chmod +x lavad
-cp lavad /usr/local/bin
+sudo ln -s $HOME/.lava/cosmovisor/genesis $HOME/.lava/cosmovisor/current -f
+sudo ln -s $HOME/.lava/cosmovisor/current/bin/lavad /usr/local/bin/lavad -f
 
-  sleep 1
-  lavad config keyring-backend test
-  lavad config chain-id $CHAIN_ID
-  lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
 
-
-sudo sed -i 's/pprof_laddr = "0\.0\.0\.0:6060"/pprof_laddr = "0\.0\.0\.0:6160"/' $HOME/.lava/config/config.toml
-sudo sed -i 's/laddr = "tcp:\/\/0\.0\.0\.0:26657"/laddr = "tcp:\/\/0\.0\.0\.0:16657"/' $HOME/.lava/config/config.toml
-sudo sed -i 's/node = "tcp:\/\/localhost:26657"/node = "tcp:\/\/localhost:16657"/' $HOME/.lava/config/client.toml
-sudo sed -i 's/address = "tcp:\/\/0\.0\.0\.0:1317"/address = "tcp:\/\/0\.0\.0\.0:1327"/' "$HOME/.lava/config/app.toml"
-sudo sed -i -e "s|address = \"0.0.0.0:9090\"|address = \"0.0.0.0:19090\"|; s|address = \"0.0.0.0:9091\"|address = \"0.0.0.0:19091\"|" $HOME/.lava/config/app.toml
-sudo sed -i 's|laddr = "tcp://0.0.0.0:26656"|laddr = "tcp://0.0.0.0:16656"|' $HOME/.lava/config/config.toml
-
-echo "[Unit]
-Description=Lava Node
+sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
+[Unit]
+Description=lava node service
 After=network-online.target
+
 [Service]
 User=$USER
-ExecStart=$(which lavad) start --home=$lavad_home_folder --p2p.seeds $seed_node
-Restart=always
-RestartSec=180
-LimitNOFILE=infinity
-LimitNPROC=infinity
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.lava"
+Environment="DAEMON_NAME=lavad"
+Environment="UNSAFE_SKIP_BACKUP=true"
+
 [Install]
-WantedBy=multi-user.target" >lavad.service
-sudo mv lavad.service /lib/systemd/system/lavad.service
-
-printGreen "Завантажуємо снепшот для прискорення синхронізації ноди..." && sleep 1
-
-SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
-curl "https://snapshots1-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
-
-rm -rf $HOME/lava-config
-
+WantedBy=multi-user.target
+EOF
 sudo systemctl daemon-reload
 sudo systemctl enable lavad
-sudo systemctl start lavad && sleep 5
 
+lavad config chain-id lava-testnet-2
+lavad config keyring-backend test
+lavad config node tcp://localhost:14457
+
+lavad init $MONIKER --chain-id lava-testnet-2
+
+curl -Ls https://snapshots.kjnodes.com/lava-testnet/genesis.json > $HOME/.lava/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/lava-testnet/addrbook.json > $HOME/.lava/config/addrbook.json
+
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@lava-testnet.rpc.kjnodes.com:14459\"|" $HOME/.lava/config/config.toml
+
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0ulava\"|" $HOME/.lava/config/app.toml
+
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
+  $HOME/.lava/config/app.toml
+
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:14458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:14457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:14460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:14456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":14466\"%" $HOME/.lava/config/config.toml
+sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:14417\"%; s%^address = \":8080\"%address = \":14480\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:14490\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:14491\"%; s%:8545%:14445%; s%:8546%:14446%; s%:6065%:14465%" $HOME/.lava/config/app.toml
+
+sed -i \
+  -e 's/timeout_commit = ".*"/timeout_commit = "30s"/g' \
+  -e 's/timeout_propose = ".*"/timeout_propose = "1s"/g' \
+  -e 's/timeout_precommit = ".*"/timeout_precommit = "1s"/g' \
+  -e 's/timeout_precommit_delta = ".*"/timeout_precommit_delta = "500ms"/g' \
+  -e 's/timeout_prevote = ".*"/timeout_prevote = "1s"/g' \
+  -e 's/timeout_prevote_delta = ".*"/timeout_prevote_delta = "500ms"/g' \
+  -e 's/timeout_propose_delta = ".*"/timeout_propose_delta = "500ms"/g' \
+  -e 's/skip_timeout_commit = ".*"/skip_timeout_commit = false/g' \
+  $HOME/.lava/config/config.toml
+
+printGreen "Завантажуємо снепшот для прискорення синхронізації ноди"
+curl -L https://snapshots.kjnodes.com/lava-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.lava
+[[ -f $HOME/.lava/data/upgrade-info.json ]] && cp $HOME/.lava/data/upgrade-info.json $HOME/.lava/cosmovisor/genesis/upgrade-info.json
+
+printGreen "Запускаємо ноду"
+sudo systemctl start lavad && sleep 3
 
 printDelimiter
 printGreen "Переглянути журнал логів:         sudo journalctl -u lavad -f -o cat"
