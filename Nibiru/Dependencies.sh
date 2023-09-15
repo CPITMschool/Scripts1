c37f8155b4e053b892b66b2779a84f5be873ed35
Asapov <137000528+asapov01@users.noreply.github.com>
2023-09-16 01:36:09 +0300

    Update Dependencies.sh

--- Nibiru/Dependencies.sh
+++ Nibiru/Dependencies.sh
@@ -7,7 +7,7 @@ function printGreen {
 printGreen "Оновлення пакетів сервера..." && sleep 1
 sudo apt-get update &&
 sudo apt-get install -y curl iptables build-essential git lz4 wget jq make gcc nano chrony \
-tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev &&
+sudo apt install -y make gcc jq curl git build-essential chrony unzip gzip
 
 printGreen "Встановлюємо GO..." && sleep 1
 if ! [ -x "$(command -v go)" ]; then
