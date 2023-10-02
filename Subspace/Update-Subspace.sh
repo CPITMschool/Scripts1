d20dc0e48d96d11976157a8d280af04db7067b01
Asapov <137000528+asapov01@users.noreply.github.com>
2023-10-02 20:55:50 +0300

    Update Update-Subspace.sh

--- Subspace/Update-Subspace.sh
+++ Subspace/Update-Subspace.sh
@@ -11,9 +11,9 @@ function printGreen {
 logo
 
 function update() {
-printGreen "Розпочалось встановлення Subpsace Gemini 3f v.0.6.10"
+printGreen "Розпочалось встановлення Subpsace Gemini 3f v.0.6.11"
 cd $HOME
-wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.10-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.10-alpha
+wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.11-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.11-alpha
 sudo chmod +x pulsar
 sudo mv pulsar /usr/local/bin/
 sudo rm -rf $HOME/.config/pulsar
@@ -24,7 +24,7 @@ sleep 1
 if [[ `service subspaced status | grep active` =~ "running" ]]; then
 echo ""
   echo "=================================================="
-  printGreen "Subspace Gemini 3f v0.6.10 успішно встановлено"
+  printGreen "Subspace Gemini 3f v0.6.11 успішно встановлено"
   echo ""
   printGreen "Корисні команди:"
   echo "Перевірити статус ноди - systemctl status subspaced"
@@ -39,7 +39,7 @@ touch $HOME/.sdd_Subspace_do_not_remove
 
 }
 
-printGreen "Під час встановлення ваша нода оновиться на актуальну мережу Gemini 3f v.0.6.10. Ви згідні? (Y/N): "
+printGreen "Під час встановлення ваша нода оновиться на актуальну мережу Gemini 3f v.0.6.11. Ви згідні? (Y/N): "
 read choice
 
 if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
