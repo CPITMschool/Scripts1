3cf85ddc75cff73d6bfdc20511172eac2b5ab286
Asapov <137000528+asapov01@users.noreply.github.com>
2023-10-19 02:02:10 +0300

    Update Install-Lava.sh

--- Lava/Install-Lava.sh
+++ Lava/Install-Lava.sh
@@ -27,67 +27,33 @@ curl -Ls https://go.dev/dl/go1.20.8.linux-amd64.tar.gz | sudo tar -xzf - -C /usr
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
-cd $HOME
-rm -rf lava
-git clone https://github.com/lavanet/lava.git
-cd lava
-git checkout v0.25.2
-
+printGreen "Встановлення Lava"
 export LAVA_BINARY=lavad
-make build
 
-mkdir -p $HOME/.lava/cosmovisor/genesis/bin
-mv build/lavad $HOME/.lava/cosmovisor/genesis/bin/
-rm -rf build
+cd || return
+rm -rf lava
+git clone https://github.com/lavanet/lava
+cd lava || return
+git checkout v0.25.2
+make install
 
-sudo ln -s $HOME/.lava/cosmovisor/genesis $HOME/.lava/cosmovisor/current -f
-sudo ln -s $HOME/.lava/cosmovisor/current/bin/lavad /usr/local/bin/lavad -f
-
-go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-
-sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
-[Unit]
-Description=lava node service
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=$(which cosmovisor) run start
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-Environment="DAEMON_HOME=$HOME/.lava"
-Environment="DAEMON_NAME=lavad"
-Environment="UNSAFE_SKIP_BACKUP=true"
-
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo systemctl daemon-reload
-sudo systemctl enable lavad
-
-lavad config chain-id lava-testnet-2
 lavad config keyring-backend test
-lavad config node tcp://localhost:14457
-
+lavad config chain-id $CHAIN_ID
 lavad init $MONIKER --chain-id lava-testnet-2
 
-curl -Ls https://snapshots.kjnodes.com/lava-testnet/genesis.json > $HOME/.lava/config/genesis.json
-curl -Ls https://snapshots.kjnodes.com/lava-testnet/addrbook.json > $HOME/.lava/config/addrbook.json
-
-sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@lava-testnet.rpc.kjnodes.com:14459\"|" $HOME/.lava/config/config.toml
-
-sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0ulava\"|" $HOME/.lava/config/app.toml
-
-sed -i \
-  -e 's|^pruning *=.*|pruning = "custom"|' \
-  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
-  $HOME/.lava/config/app.toml
+curl -s https://raw.githubusercontent.com/lavanet/lava-config/main/testnet-2/genesis_json/genesis.json > $HOME/.lava/config/genesis.json
+curl -s https://snapshots-testnet.nodejumper.io/lava-testnet/addrbook.json > $HOME/.lava/config/addrbook.json
 
 sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:14458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:14457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:14460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:14456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":14466\"%" $HOME/.lava/config/config.toml
 sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:14417\"%; s%^address = \":8080\"%address = \":14480\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:14490\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:14491\"%; s%:8545%:14445%; s%:8546%:14446%; s%:6065%:14465%" $HOME/.lava/config/app.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.lava/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.lava/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.lava/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.lava/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025ulava"|g' $HOME/.lava/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.lava/config/config.toml
 
 sed -i \
   -e 's/timeout_commit = ".*"/timeout_commit = "30s"/g' \
@@ -98,14 +64,32 @@ sed -i \
   -e 's/timeout_prevote_delta = ".*"/timeout_prevote_delta = "500ms"/g' \
   -e 's/timeout_propose_delta = ".*"/timeout_propose_delta = "500ms"/g' \
   -e 's/skip_timeout_commit = ".*"/skip_timeout_commit = false/g' \
+  -e 's/seeds = ".*"/seeds = "3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@testnet2-seed-node2.lavanet.xyz:26656"/g' \
   $HOME/.lava/config/config.toml
+sed -i -e 's/broadcast-mode = ".*"/broadcast-mode = "sync"/g' $HOME/.lava/config/config.toml
 
+sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
+[Unit]
+Description=Lava Network Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which lavad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book
 printGreen "Завантажуємо снепшот для прискорення синхронізації ноди"
-curl -L https://snapshots.kjnodes.com/lava-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.lava
-[[ -f $HOME/.lava/data/upgrade-info.json ]] && cp $HOME/.lava/data/upgrade-info.json $HOME/.lava/cosmovisor/genesis/upgrade-info.json
-
+SNAP_NAME=$(curl -s https://snapshots-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
+curl "https://snapshots-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
 printGreen "Запускаємо ноду"
-sudo systemctl start lavad && sleep 3
+sudo systemctl daemon-reload
+sudo systemctl enable lavad
+sudo systemctl start lavad
 
 printDelimiter
 printGreen "Переглянути журнал логів:         sudo journalctl -u lavad -f -o cat"
@@ -113,6 +97,7 @@ printGreen "Переглянути статус синхронізації: lava
 printGreen "Порти які використовує ваша нода: 14458,14457,14460,14456,14466,14417,14480,14490,14491"
 printGreen "В журналі логів спочатку ви можете побачити помилку Connection is closed. Але за 5-10 секунд нода розпочне синхронізацію"
 printDelimiter
+
 
 }
 install
