edb0bf2056ad2e4d00a279eb0a8c3d9ba180546f
Asapov <137000528+asapov01@users.noreply.github.com>
2023-10-26 01:05:45 +0300

    Update Update-Lava.sh

--- Lava/Update-Lava.sh
+++ Lava/Update-Lava.sh
@@ -12,14 +12,13 @@ function backup_files() {
   source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
   printGreen "Копіюємо бекап файли ноди Lava в папку /root/BACKUPNODES/Lava backup" && sleep 3
   backup_dir="$HOME/BACKUPNODES"
-  if [ ! -d "$backup_dir" ]; then
-    mkdir -p "$backup_dir"
-  fi
-  cp "$HOME/.lava/data/priv_validator_state.json" "$backup_dir/Lava backup/"
-  cp "$HOME/.lava/config/node_key.json" "$backup_dir/Lava backup/"
-  cp "$HOME/.lava/config/priv_validator_key.json" "$backup_dir/Lava backup/" 
+  sub_dir="$backup_dir/Lava backup"
+  mkdir -p "$sub_dir"
+  cp "$HOME/.lava/data/priv_validator_state.json" "$sub_dir/"
+  cp "$HOME/.lava/config/node_key.json" "$sub_dir/"
+  cp "$HOME/.lava/config/priv_validator_key.json" "$sub_dir/" 
   echo "Збережено: $lava_file_to_copy" && sleep 3
-}
+} 
 
 
 function remove_node() {
