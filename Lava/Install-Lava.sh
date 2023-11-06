b499ff2823089e8f6c5aefe3642f433fa35a11f6
Asapov <137000528+asapov01@users.noreply.github.com>
2023-11-06 20:17:52 +0200

    Update Install-Lava.sh

--- Lava/Install-Lava.sh
+++ Lava/Install-Lava.sh
@@ -21,11 +21,7 @@ function install() {
   sudo apt -qy upgrade
 
   printGreen "Встановлення Go"
-  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-  sudo rm -rf /usr/local/go
-  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-  rm "go$ver.linux-amd64.tar.gz"
-  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+  source <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/go_install.sh")
   source $HOME/.bash_profile
 
   printGreen "Встановлення Lava"
