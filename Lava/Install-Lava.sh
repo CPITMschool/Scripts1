114c0c33c1bb684c764db9e7241f26bb9e113bcd
Asapov <137000528+asapov01@users.noreply.github.com>
2023-11-15 00:00:52 +0200

    Update Install-Lava.sh

--- Lava/Install-Lava.sh
+++ Lava/Install-Lava.sh
@@ -15,16 +15,15 @@ function install() {
   printGreen "Введіть ім'я для вашої ноди(Наприклад:Oliver):"
   read -r NODE_MONIKER
 
+  CHAIN_ID="lava-testnet-2"
+  CHAIN_DENOM="ulava"
+  BINARY_NAME="lavad"
+  BINARY_VERSION_TAG="v0.27.0"
   printGreen "Встановлення необхідних залежностей"
-  sudo apt -q update
-  sudo apt -qy install curl git jq lz4 build-essential
-  sudo apt -qy upgrade
+  sudo apt update
+  sudo apt install -y curl git jq lz4 build-essential unzip
 
-  printGreen "Встановлення Go"
-  sudo rm -rf /usr/local/go
-  curl -Ls https://go.dev/dl/go1.20.8.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-  eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
-  eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+  bash <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/go_install.sh")
   source .bash_profile
 
   printGreen "Встановлення Lava"
@@ -34,28 +33,17 @@ function install() {
   rm -rf lava
   git clone https://github.com/lavanet/lava
   cd lava || return
-  git checkout v0.26.1
+  git checkout v0.27.0
   make install
 
   lavad config keyring-backend test
-  lavad config chain-id lava-testnet-2
-  lavad init "$NODE_MONIKER" --chain-id lava-testnet-2
+  lavad config chain-id $CHAIN_ID
+  lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
   curl -s https://raw.githubusercontent.com/lavanet/lava-config/main/testnet-2/genesis_json/genesis.json > $HOME/.lava/config/genesis.json
   curl -s https://snapshots-testnet.nodejumper.io/lava-testnet/addrbook.json > $HOME/.lava/config/addrbook.json
 
-  sed -i.bak -e "s%proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:17658\"%" \
-  -e "s%laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:17656\"%" \
-  -e "s%laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:17657\"%" \
-  -e "s%pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:1760\"%" \
-  -e "s%prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":17660\"%" $HOME/.lava/config/config.toml
-
-  sed -i.bak -e "s%node = \"tcp://localhost:26657\"%node = \"tcp://localhost:17657\"%" $HOME/.lava/config/client.toml
-
-  sed -i.bak -e "s%localhost:9090%localhost:1790%" $HOME/.lava/config/app.toml
-  sed -i.bak -e "s%address = \"localhost:9091\"%address = \"localhost:1791\"%" $HOME/.lava/config/app.toml
-  sed -i.bak -e "s%address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1717\"%" $HOME/.lava/config/app.toml
-
+  SEEDS="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@testnet2-seed-node2.lavanet.xyz:26656"
   PEERS=""
   sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.lava/config/config.toml
 
@@ -79,9 +67,11 @@ function install() {
     -e 's/seeds = ".*"/seeds = "3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@testnet2-seed-node2.lavanet.xyz:26656"/g' \
     $HOME/.lava/config/config.toml
 
-  sed -i -e 'bcast_mode = ".*"/bcast_mode = "sync"/g' $HOME/.lava/config/config.toml
+  sed -i -e 's/broadcast-mode = ".*"/broadcast-mode = "sync"/g' $HOME/.lava/config/config.toml
 
-sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
+  printCyan "5. Starting service and synchronization..." && sleep 1
+
+  sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
 [Unit]
 Description=Lava Network Node
 After=network-online.target
@@ -95,22 +85,21 @@ LimitNOFILE=10000
 WantedBy=multi-user.target
 EOF
 
-lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book
+  lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book
 
-printGreen "Завантажуємо снепшот для прискорення синхронізації ноди"
-SNAP_NAME=$(curl -s https://snapshots-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
-curl "https://snapshots-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
-printGreen "Запускаємо ноду"
-sudo systemctl daemon-reload
-sudo systemctl enable lavad
-sudo systemctl start lavad
+  SNAP_NAME=$(curl -s https://snapshots-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
+  curl "https://snapshots-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
 
-printDelimiter
-printGreen "Переглянути журнал логів:         sudo journalctl -u lavad -f -o cat"
-printGreen "Переглянути статус синхронізації: lavad status 2>&1 | jq .SyncInfo"
-printGreen "Порти які використовує ваша нода: 17658,17657,17656,1760,17660,1790,1791,1717"
-printGreen "В журналі логів спочатку ви можете побачити помилку Connection is closed. Але за 5-10 секунд нода розпочне синхронізацію"
-printDelimiter
+  printGreen "Запускаємо ноду"
+  sudo systemctl daemon-reload
+  sudo systemctl enable lavad
+  sudo systemctl start lavad
+
+  printDelimiter
+  printGreen "Переглянути журнал логів:         sudo journalctl -u lavad -f -o cat"
+  printGreen "Переглянути статус синхронізації: lavad status 2>&1 | jq .SyncInfo"
+  printGreen "В журналі логів спочатку ви можете побачити помилку Connection is closed. Але за 5-10 секунд нода розпочне синхронізацію"
+  printDelimiter
 }
 
 install
