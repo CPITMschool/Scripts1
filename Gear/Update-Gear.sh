26bda1f705a861211592d63d8d4edd892fa110de
Asapov <137000528+asapov01@users.noreply.github.com>
2023-11-24 13:32:47 +0200

    Update Update-Gear.sh

--- Gear/Update-Gear.sh
+++ Gear/Update-Gear.sh
@@ -1,64 +1,33 @@
 #!/bin/bash
 
-function logo() {
-    bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+function printDelimiter {
+  echo "==========================================="
 }
 
 function printGreen {
-    echo -e "\e[1m\e[32m${1}\e[0m"
+  echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
-logo
+function install() {
+  clear
+  source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+  printGreen "Оновлюємо Zetachain"
+  echo ""
+  sudo systemctl stop zetacored
+  curl -L https://github.com/zeta-chain/node/releases/download/v10.1.0/zetacored_testnet-linux-amd64 > $HOME/go/bin/zetacored
+  chmod +x $HOME/go/bin/zetacored
 
-gear_path=$(find /root -name "gear" -type f)
-gear_version=$($gear_path --version)
-printGreen "Версія вашої ноди Gear - $gear_version"
-
-printGreen "Актуальна версія для оновлення 0.3.1. Бажаєте оновити? (Y/N)"
-read response
-
-function update() {
-    if [[ $response == "Y" || $response == "y" ]]; then
-        echo "==================================================="
-        printGreen "Розпочалось оновлення вашої ноди"
-        echo "==================================================="
-        cd
-        sudo systemctl stop gear
-        
-        printGreen "Завантаження нової версії на ваш сервер"
-       curl https://get.gear.rs/gear-v0.3.1-x86_64-unknown-linux-gnu.tar.xz | sudo tar -xJC /root
-
-
-        sudo systemctl stop gear
-        /root/gear purge-chain -y && sleep 3
-
-        printGreen "Перезавантажуємо Gear."
-        sudo systemctl start gear
-        sleep 10
-
-        sed -i "s/gear-node/gear/" "/etc/systemd/system/gear.service"
-
-        sudo systemctl daemon-reload
-        sudo systemctl stop gear
-
-        cd /root/.local/share/gear/chains
-        mkdir -p gear_staging_testnet_v6/network/
-
-        sudo cp gear_staging_testnet_v6/network/secret_ed25519 gear_staging_testnet_v7/network/secret_ed25519 &>/dev/null
-
-        sudo sed -i 's/telemetry\.postcapitalist\.io/telemetry.doubletop.io/g' /etc/systemd/system/gear.service
-
-        sudo systemctl daemon-reload
-        sudo systemctl restart gear && sleep 10
-
-        gear_path=$(find /root -name "gear" -type f)
-        updated_gear_version=$($gear_path --version)
-        printGreen "Оновлення Gear до версії $updated_gear_version завершено."
-        printGreen "Перевірити поточну версію вашої ноди ви можете командою - ./gear --version . "
-    else
-        printGreen "Оновлення Gear не виконано.Спробуйте ще раз."
-    fi
+  sudo systemctl start zetacored
+  sleep 2
+  printGreen "Версія вашої ноди:"
+  zetacored version
+  echo ""
+  
+  printDelimiter
+  printGreen "Переглянути журнал логів:         sudo journalctl -u zetacored -f -o cat"
+  printGreen "Переглянути статус синхронізації: zetacored status 2>&1 | jq .SyncInfo"
+  printGreen "В журналі логів спочатку ви можете побачити помилку Connection is closed. Але за 5-10 секунд нода розпочне синхронізацію"
+  printDelimiter
 }
 
-
-update
+install
