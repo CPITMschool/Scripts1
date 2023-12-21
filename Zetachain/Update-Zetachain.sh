398dddd5b5c08c9a7565673c587d0dc2edbb94cd
Asapov <137000528+asapov01@users.noreply.github.com>
2023-12-21 02:01:05 +0200

    Update Update-Zetachain.sh

--- Zetachain/Update-Zetachain.sh
+++ Zetachain/Update-Zetachain.sh
@@ -18,7 +18,6 @@ function install() {
   mkdir -p $HOME/go/bin
   curl -L https://github.com/zeta-chain/node/releases/download/v11.0.0-rc/zetacored-linux-amd64 > $HOME/go/bin/zetacored
   chmod +x $HOME/go/bin/zetacored
-
   sudo systemctl start zetacored
   sleep 2
   printGreen "Версія вашої ноди:"
