d3b838820ad7a8beb56c3d2b5db71b3574592111
CPITMschool <cpi.tm.school@gmail.com>
2024-01-08 20:21:22 +0200

    X2 Update
    Update Babylon & Dymension

---
+++ Babylon/Delete-Babylon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+function logo() {
+  bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+}
+
+function delete() {
+  rm -rf $HOME/babylon
+  rm -rf $HOME/.babylond
+  rm /etc/systemd/system/babylond.service
+}
+
+logo
+delete
+
+printGreen "Babylon node - видалено"
