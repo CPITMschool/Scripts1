a7fca94f3ee015a23860ba414b3679d3889207b8
Asapov <137000528+asapov01@users.noreply.github.com>
2024-01-26 11:53:57 +0200

    Update Update-Zetachain.sh

--- Zetachain/Update-Zetachain.sh
+++ Zetachain/Update-Zetachain.sh
@@ -16,7 +16,7 @@ function install() {
   sudo systemctl stop zetacored
 
   mkdir -p $HOME/go/bin
-  curl -L https://github.com/zeta-chain/node/releases/download/v12.0.0-rc/zetacored-linux-amd64 > $HOME/go/bin/zetacored
+  curl -L https://github.com/zeta-chain/node/releases/download/v12.1.0/zetacored-linux-amd64 > $HOME/go/bin/zetacored
   chmod +x $HOME/go/bin/zetacored
   sudo systemctl start zetacored
   sleep 2
