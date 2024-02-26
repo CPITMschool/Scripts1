57e6b0de58d923f495610c2a9ba455ec5365c353
Asapov <137000528+asapov01@users.noreply.github.com>
2024-02-26 20:16:48 +0200

    Update Install-Babylon.sh

--- Babylon/Install-Babylon.sh
+++ Babylon/Install-Babylon.sh
@@ -20,16 +20,10 @@ function install() {
   sudo apt update
   sudo apt install -y curl git jq lz4 build-essential unzip
 
-  if ! command -v go &> /dev/null; then
   bash <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/go_install.sh")
-else
-  echo "Go вже встановлено."
-fi
+  source ~/.bash_profile
 
-source ~/.bash_profile
-
-  cd $HOME
-  rm -rf babylon
+  cd && rm -rf babylon
   git clone https://github.com/babylonchain/babylon
   cd babylon
   git checkout v0.8.3
@@ -38,6 +32,8 @@ source ~/.bash_profile
 
   babylond config chain-id bbn-test-3
   babylond config keyring-backend test
+  babylond config client node tcp://localhost:20657
+
   babylond init $NODE_MONIKER --chain-id bbn-test-3
 
   curl -L https://snapshots-testnet.nodejumper.io/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
@@ -56,27 +52,12 @@ sed -i \
   -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
   $HOME/.babylond/config/app.toml
 
-
 sed -i 's|^network *=.*|network = "signet"|g' $HOME/.babylond/config/app.toml
 
+sed -i -e "s%:1317%:20617%; s%:8080%:20680%; s%:9090%:20690%; s%:9091%:20691%; s%:8545%:20645%; s%:8546%:20646%; s%:6065%:20665%" $HOME/.babylond/config/app.toml
+sed -i -e "s%:26658%:20658%; s%:26657%:20657%; s%:6060%:20660%; s%:26656%:20656%; s%:26660%:20661%" $HOME/.babylond/config/config.toml
 
 curl "https://snapshots-testnet.nodejumper.io/babylon-testnet/babylon-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.babylond"
-
-#Change port #7
-sed -i.bak -e "s%:1317%:2017%g; 
-s%:8080%:8780%g;
-s%:9090%:9790%g;
-s%:9091%:9791%g;
-s%:8545%:9245%g;
-s%:8546%:9246%g;
-s%:6065%:6765%g" $HOME/.babylond/config/app.toml
-sed -i.bak -e "s%:26658%:33658%g;
-s%:26657%:33657%g;
-s%:6060%:6760%g;
-s%tcp://0.0.0.0:26656%tcp://0.0.0.0:33656%g;
-s%:26660%:33660%g" $HOME/.babylond/config/config.toml
-sed -i.bak -e "s%:26657%:33657%g" $HOME/.babylond/config/client.toml
-
 
 sudo tee /etc/systemd/system/babylond.service > /dev/null << EOF
 [Unit]
@@ -93,7 +74,7 @@ WantedBy=multi-user.target
 EOF
 sudo systemctl daemon-reload
 sudo systemctl enable babylond.service
-source .bash_profile
+source ~/.bash_profile
 
 printGreen "Запускаємо ноду"
 sudo systemctl start babylond.service
@@ -101,9 +82,9 @@ sudo systemctl start babylond.service
 printDelimiter
 printGreen "Переглянути журнал логів:         sudo journalctl -u babylond -f -o cat"
 printGreen "Переглянути статус синхронізації: babylond status 2>&1 | jq .SyncInfo"
-echo -e "\e[1;32mВаша нода Babylon займає наступний набір портів(#7):\e[0m 2017,8780,9790,9791,9245,9246,6765,33658,33657,6760,33656,33660"
+echo -e "\e[1;32mВаша нода Babylon займає наступний набір портів:\e[0m 20617,20680,20690,20691,20645,20646,20665,20658,20657,20656,20660"
 printGreen "Запишіть значення портів, для можливості подальшого підселення космос нод, та уникнення конфліктів пов'язаних з портами"
-echo "alias port_babylon='echo -e \"\e[1;32mВаша нода займає наступний набір портів(#7):\e[0m 2017,8780,9790,9791,9245,9246,6765,33658,33657,6760,33656,33660\"'" >> ~/.bash_profile
+echo "alias port_babylon='echo -e \"\e[1;32mВаша нода займає наступний набір портів:\e[0m 20617,20680,20690,20691,20645,20646,20665,20658,20657,20656,20660\"'" >> ~/.bash_profile
 printGreen "Також якщо вам колись буде потрібно, введіть в терміналі port_babylon та ви знову побачите інформацію про список зайнятих портів"
 printDelimiter
 source ~/.bash_profile
